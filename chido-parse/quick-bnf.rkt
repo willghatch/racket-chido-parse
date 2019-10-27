ff49af2cc11281c731000d9dc8dc9336b0263268
William G Hatch <william@hatch.uno>
2019-10-26 18:27:43 -0600

    work on quick-bnf hash-lang

--- chido-parse/quick-bnf.rkt
+++ chido-parse/quick-bnf.rkt
@@ -8,6 +8,7 @@
   (require
    rackunit
    racket/stream
+   "private/test-util.rkt"
    ))
 
 #|
@@ -30,18 +31,78 @@
                               id-parser)))
                 '("test_id")))
 
-(define-bnf cpqb-parser
-  [top-level [[: #:repeat-min 1 arm]]]
+(define string-parser
+  (proc-parser #:name "string-parser"
+               #:prefix "\""
+               #:preserve-prefix? #t
+               #:promise-no-left-recursion? #t
+               (λ (port)
+                 (read-syntax (object-name port) port))))
+
+(define-bnf/quick cpqb-parser
+  [top-level [arm +]]
   [arm [id-parser ":" alt-sequence]]
-  [alt-sequence [alt [: #:splice 1 #:repeat-min 0
-                        (binding-sequence [: #:ignore #t "|"] alt)]]]
-  [alt [[: #:repeat-min 1 elem] [: #:repeat-min 0 alt-flag]]]
-  [alt-flag (TODO -- IE assoc/precidence)]
-  [elem [[: #:splice 1 #:repeat-max 1 (binding-sequence id-parser "=")]
-         [: #:repeat-max 1 "/"]
-         [: #:repeat-min 0 "@"]
-         base-parser ;; TODO - this should have alt capabilities -- eg. (a | b)
-         [: #:repeat-max 1 "?"]
-         [: #:repeat-max 1 "+"]
-         [: #:repeat-max 1 "*"]]]
+  [alt-sequence [alt @ #(/ "|" alt) +]]
+  [alt [elem + alt-flag *]]
+  [alt-flag ["&" (|| "left" "right")]
+            ;; TODO - associativity groups
+            ["<" (|| string-parser
+                     #(/"(" string-parser + /")"))]
+            [">" (|| string-parser
+                     #(/"(" string-parser + /")"))]]
+  [elem [@ #(id-parser "=") ?
+         @ "/" ?
+         @ "@" *
+         compound-parser
+         @ "?" ?
+         @ "*" ?
+         @ "+" ?]]
+  [compound-parser [id-parser]
+                   [string-parser]
+                   ;[lisp-escape-parser]
+                   ["(" elem #("|" elem) + ")"]
+                   ["(" elem + ")"]]
+  )
+
+(module+ test
+  (define bnf-string-1 "
+stmt: \"pass\"
+")
+
+  ;; TODO - figuring out what's wrong probably requires another pass at working with failures... probably just figure out the rightmost failure at each juncture and only propagate that.
+  #;(eprintf "got: ~v\n"
+           (parse* (open-input-string bnf-string-1) cpqb-parser))
+
+
+  (define bnf-string/stmt "
+stmt : \"pass\"
+     | expression
+     | \"{\" stmt + \"}\"
+expr : bnumber
+     | expr \"+\" expr & left
+     | expr \"*\" expr & left > +
+bnumber : (\"0\" | \"1\") +
+")
+
+  #;(eprintf "result: ~v\n"
+           (whole-parse* (open-input-string bnf-string/stmt) cpqb-parser))
+
+  (check se/datum?
+         (map parse-derivation-result
+              (stream->list
+               (whole-parse* (open-input-string bnf-string/stmt)
+                             cpqb-parser)))
+         (list #'([stmt ":"
+                        {(["pass"]) ()}
+                        {([expression]) ()}
+                        {(["{"] [stmt "+"] ["}"]) ()}
+                        ]
+                  [expr ":"
+                        {([bnumber]) ()}
+                        {([expr] ["+"] [expr]) (["&" "left"])}
+                        {([expr] ["*"] [expr]) (["&" "left"] [">" "+"])}
+                        ]
+                  [bnumber ":"
+                           {([{"(" "0" "|" "1" ")"}])}]
+                  )))
   )
