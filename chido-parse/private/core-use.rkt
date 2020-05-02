4afd08d8b7aac9d6498e425fbf3e05e053ca7889
William G Hatch <william@hatch.uno>
2020-05-02 16:16:11 -0600

    core-use: shorten so only one line needs to be commented

--- chido-parse/private/core-use.rkt
+++ chido-parse/private/core-use.rkt
@@ -1,6 +1,4 @@
 #lang racket/base
 
-(require "core.rkt")
-(provide (all-from-out "core.rkt"))
-;(require "core-direct-jumps-rewrite.rkt")
-;(provide (all-from-out "core-direct-jumps-rewrite.rkt"))
+(require "core.rkt") (provide (all-from-out "core.rkt"))
+;(require "core-direct-jumps-rewrite.rkt") (provide (all-from-out "core-direct-jumps-rewrite.rkt"))
